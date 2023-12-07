9e6b196bc28e9bb79a92f7b2ba2c86927c76d28f
wanghao <wanghao@casachina.com.cn>
2023-12-07 17:01:09 +0800

    Update esapp exec path

--- internal/defconst.go
+++ internal/defconst.go
@@ -3,7 +3,7 @@ package internal
 const (
 	defaultAccKey   = "DSF34DF145AQ981248ASD1OA"
 	defaultOutDir   = "/tmp/esh"
-	defaultExecDir  = "/usr/local/bin/"
+	defaultExecDir  = "/tmp/obs-sh"
 	defaultExecName = "es_app"
 )
 
